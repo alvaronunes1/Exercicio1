ddf14cb7ad9cadad8b9de9925781b662aba671e6
vizin <alvaro.nunes15@gmail.com>
2023-09-01 23:12:35 -0300

    feat: cria um vetor que mostra um número buscado em qual posição esta

---
+++ src/BuscaSequencial.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class BuscaSequencial {
+    public static void main(String[] args) {
+
+        int[] vetor = new int[10];
+        int busca, achado = -1;
+
+        Random randomico = new Random();
+        Scanner ler = new Scanner(System.in);
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = randomico.nextInt(10);
+        }
+
+        System.out.println("Digite um numero para ser buscado no vetor");
+        busca = ler.nextInt();
+
+        for (int i = 0; i < vetor.length; i++) {
+
+            if (vetor[i] == busca) {
+                achado = i;
+                break;
+            }
+        }
+
+        System.out.println("Vetor randomico: ");
+
+        for(int num:vetor)
+            System.out.println(num);
+
+        if (achado != -1){
+            System.out.println("Número " + busca + " achado na posição " + achado);
+        } else {
+            System.out.println("Número " + busca + " não foi achado em nenhuma posição");
+        }
+
+    }
+}
